40425690bdfe032db88a2a23f5d53d2311736676
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-03-13 21:28:33 -0700

    configure pwsh profile
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- install_wsl_ubuntu.sh
+++ install_wsl_ubuntu.sh
@@ -14,3 +14,11 @@ SCRIPT_DIR="$(dirname "$0")"
 "${SCRIPT_DIR}/wsl/setup_wsl.sh"
 "${SCRIPT_DIR}/git/configure_git.sh"
 "${SCRIPT_DIR}/wsl/install_pwsh.sh"
+
+gh repo clone brooke-hamilton/dotfiles "${HOME}/dotfiles"
+
+mkdir -p ~/.config/powershell
+
+# Create the PowerShell profile file and write the import command to it
+# shellcheck disable=SC2016
+echo '. ${HOME}/dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1' > ~/.config/powershell/Microsoft.PowerShell_profile.ps1
