ced0ecd20ede1b2d85e443f2b1d0bfd64b37f445
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-05-24 00:45:09 -0700

    delete workflow script
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- radius/delete-workflow-runs.sh
+++ radius/delete-workflow-runs.sh
@@ -9,10 +9,12 @@ if [ $# -eq 0 ]; then
 fi
 
 REPO="$1"
+GH_TOKEN="$(gh auth token)"
+export GH_TOKEN
 
 while true; do
     # Get a batch of workflow run IDs
-    run_ids=$(gh run list --repo "$REPO" -L 10 --json databaseId --jq '.[].databaseId')
+    run_ids=$(gh run list --repo "$REPO" -L 30 --json databaseId --jq '.[].databaseId')
     
     # Check if we got any results
     if [[ -z "$run_ids" ]]; then
@@ -23,7 +25,13 @@ while true; do
     # Process each ID in the current batch
     echo "$run_ids" | while read id; do
         echo "Deleting workflow run with ID: $id"
-        gh run delete --repo "$REPO" "$id"
+        # The gh CLI command is simpler but much slower than using curl
+        #gh run delete --repo "$REPO" "$id"
+        curl -sL -X DELETE \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer $GH_TOKEN" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            "https://api.github.com/repos/$REPO/actions/runs/$id"
     done
     
     echo "Batch completed. Checking for more workflow runs..."
