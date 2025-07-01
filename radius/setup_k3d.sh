6c801f21b0d5a7ab26ef9773fb561eb8606609a2
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-07-01 18:58:11 -0400

    go on devcontainer
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- radius/setup_k3d.sh
+++ radius/setup_k3d.sh
@@ -11,4 +11,5 @@ k3d cluster create -p "8081:80@loadbalancer" \
 if command -v rad >/dev/null 2>&1; then
     rad install kubernetes --set rp.publicEndpointOverride=localhost:8081
     rad workspace create kubernetes k3d --context k3d-k3s-default --force
+    rad version
 fi
