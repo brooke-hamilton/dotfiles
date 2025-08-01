9784db977dc5d393127cba9cd7a63d9ac60e9211
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-07-31 21:35:37 -0400

    terraform
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- wsl/setup_wsl_apt.sh
+++ wsl/setup_wsl_apt.sh
@@ -46,3 +46,8 @@ sudo apt-get install aspell aspell-en -y
 
 # Postgres client
 sudo apt installp ostgresql-client -y
+
+# Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
