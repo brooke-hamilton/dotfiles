47583e7e08dcff0cbf906a86d9bd3d7a9afe9872
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-08-04 22:55:56 -0400

    app delete test
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

---
+++ radius/test/app-delete-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# Create a group with one environment and one application
+rad group create group1
+rad env create env1 --group group1
+rad deploy app.bicep -g group1 -e env1
+
+echo ""
+echo "Applications deployed successfully. Now deleting..."
+
+# Delete applications from each environment
+rad app delete todoapp -y
+
+# Delete environments
+rad env delete env1 --group group1 --yes
+
+# Delete groups
+rad group delete group1 --yes
+
+echo "Cleanup completed successfully."
