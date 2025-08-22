786bd21e6ce4e325528c2fb136e4c58ab2f97541
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-08-22 10:10:33 -0400

    k3d install
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- radius/setup_k3d.sh
+++ radius/setup_k3d.sh
@@ -5,6 +5,12 @@ set -e
 
 k3d cluster delete
 k3d cluster create \
-    -p "8081:80@loadbalancer" \
+    --port "8081:80@loadbalancer" \
     --k3s-arg "--disable=traefik@server:*" \
-    --k3s-arg "--disable=servicelb@server:*"
+    --k3s-arg "--disable=servicelb@server:*" \
+    --wait
+
+if command -v rad >/dev/null 2>&1; then
+    rad install kubernetes \
+        --set rp.publicEndpointOverride=localhost:8081
+fi
