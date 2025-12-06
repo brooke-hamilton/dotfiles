1c2c1b8ce935fcf512ae6bfcd4a07df4885338b7
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-12-06 12:44:40 -0500

    dotnet 10
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- wsl/setup-dotnet-apt.sh
+++ wsl/setup-dotnet-apt.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
+cleanup() {
+    rm -f packages-microsoft-prod.deb dotnet-install.sh
+}
+
+trap cleanup EXIT
+
+# Install .NET 8 SDK via apt
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
 
-sudo apt-get update &&
-    sudo apt-get install -y dotnet-sdk-8.0
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-8.0
+
+# Install .NET 10 SDK via dotnet-install.sh
+# Temporarily use manual installation due to this issue: https://github.com/dotnet/docs/issues/50349#issuecomment-3618807570
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+sudo bash dotnet-install.sh --version 10.0.100 --install-dir /usr/share/dotnet
