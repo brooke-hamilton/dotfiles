35efb03990a00a788e93b3d2cd05e964e9e545a3
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-12-10 17:26:41 -0500

    go installer with oobe
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- wsl/setup_go.sh
+++ wsl/setup_go.sh
@@ -10,24 +10,18 @@ echo "Setting up Go version ${GO_VERSION}..."
 
 # Download and install Go
 wget -q https://golang.org/dl/${GO_FILE}
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf ${GO_FILE}
+rm -rf /usr/local/go
+tar -C /usr/local -xzf ${GO_FILE}
 rm ${GO_FILE}
 
 # shellcheck disable=SC2016
-echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile >/dev/null
-# shellcheck disable=SC2016
-echo 'export PATH=$PATH:$(go env GOPATH)/bin' >>~/.bashrc
-# shellcheck disable=SC1091
-#. /etc/profile
-# shellcheck disable=SC1090
-#. ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a /etc/profile >/dev/null
 
 # Tools
-go install gotest.tools/gotestsum@latest
-go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.0
-go install go.uber.org/mock/mockgen@v0.4.0
-go install github.com/stern/stern@latest
+# go install gotest.tools/gotestsum@latest
+# go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.0
+# go install go.uber.org/mock/mockgen@v0.4.0
+# go install github.com/stern/stern@latest
 
-# golanci-lint: https://golangci-lint.run/welcome/install/#binaries
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.64.6
+# # golanci-lint: https://golangci-lint.run/welcome/install/#binaries
+# curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.64.6
