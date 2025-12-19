031a35c707289ef4efea60c806ea58bd056d2d1e
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-12-19 14:08:06 -0500

    update
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- install.ps1
+++ install.ps1
@@ -1,18 +1,12 @@
 #Requires -RunAsAdministrator
 
-# Make sure the submodule was cloned with this repo.
-if((Get-ChildItem -Path "$PSScriptRoot\submodules\radius-dev-config" | Measure-Object).Count -eq 0) {
-    Write-Output "Clone the submodule with this repo. Run: git submodule update --init --recursive"
-    exit
-}
-
 # Enable winget configuration
 winget configure --enable
 
 # Personal preferences
 . "$PSScriptRoot\.configurations\Set-WinGetConfiguration.ps1" -YamlConfigFilePath "$PSScriptRoot\.configurations\desktop-settings.dsc.yaml"
-. "$PSScriptRoot\.configurations\Set-WinGetConfiguration.ps1" -YamlConfigFilePath "$PSScriptRoot\.configurations\apps-to-remove.dsc.yaml"
-. "$PSScriptRoot\.configurations\Set-WinGetConfiguration.ps1" -YamlConfigFilePath "$PSScriptRoot\.configurations\apps.dsc.yaml"
+dsc config set --file "$PSScriptRoot\.configurations\apps-to-remove.dsc.yaml"
+dsc config set --file "$PSScriptRoot\.configurations\apps.dsc.yaml"
 . "$PSScriptRoot\.configurations\Set-WinGetConfiguration.ps1" -YamlConfigFilePath "$PSScriptRoot\.configurations\office-apps.dsc.yaml"
 
 # Radius setup
