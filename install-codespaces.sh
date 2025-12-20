3a213c0ced417e4c3ad56065ecf6833aeaa015f5
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2025-12-20 09:19:57 -0500

    redundant setup
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- install-codespaces.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install k3d: https://k3d.io/stable/#installation
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
--- install-devcontainer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Additional configuration for dev containers.
-
-echo "Dev container setup from dotfiles complete."
