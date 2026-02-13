33282684b873fd4d128205f071641dea104ab686
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2026-02-13 10:09:06 -0500

    go install
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- wsl/setup_go.sh
+++ wsl/setup_go.sh
@@ -3,26 +3,31 @@
 set -e
 
 # Script to set up Go environment modified from https://go.dev/doc/install
-GO_VERSION="1.25.0"
+GO_VERSION="1.26.0"
 GO_FILE="go${GO_VERSION}.linux-amd64.tar.gz"
 
 echo "Setting up Go version ${GO_VERSION}..."
 
 # Download and install Go
-wget -q https://golang.org/dl/${GO_FILE}
+wget -q "https://golang.org/dl/${GO_FILE}"
 rm -rf /usr/local/go
-tar -C /usr/local -xzf ${GO_FILE}
-rm ${GO_FILE}
+tar -C /usr/local -xzf "${GO_FILE}"
+rm "${GO_FILE}"
 
 # shellcheck disable=SC2016
 echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a /etc/profile >/dev/null
 
 # Tools
-# go install gotest.tools/gotestsum@latest
-# go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.0
-# go install go.uber.org/mock/mockgen@v0.4.0
-# go install github.com/stern/stern@latest
+echo "Installing Go tools..."
+go install sigs.k8s.io/controller-tools/cmd/controller-gen@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/suzuki-shunsuke/ghalint/cmd/ghalint@latest
+go install golang.org/x/tools/gopls@latest
+go install gotest.tools/gotestsum@latest
+go install go.uber.org/mock/mockgen@latest
+go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/stern/stern@latest
 
-# # golanci-lint: https://golangci-lint.run/welcome/install/#binaries
-# curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.64.6
+# golangci-lint: https://golangci-lint.run/welcome/install/#binaries
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.64.6
